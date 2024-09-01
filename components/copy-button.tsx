07ba91b66af04c4c17e11676b3c5d0cccafcfccf
wandreperes <wandreperes@yahoo.com.br>
2024-09-01 19:27:33 -0300

    New Homepage

---
+++ components/copy-button.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useCopyToClipboard } from '@uidotdev/usehooks'
+import { Check, Copy } from 'lucide-react'
+
+import Image from 'next/image'
+
+export function CopyButton() {
+  const [copiedText, copyToClipboard] = useCopyToClipboard()
+  const hasCopiedText = Boolean(copiedText)
+  return (
+    <div
+      onClick={() => copyToClipboard('11974417380')}
+      className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer'
+    >
+      <h2 className='mb-3 text-2xl font-semibold'>
+        Contato
+        <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'></span>
+      </h2>
+      <p className='m-0 max-w-[30ch] text-sm opacity-50 flex gap-2 '>
+        <Image src='/whatsapp.png' alt='WhatsApp' width={20} height={20} /> (11) 9 7441-7380
+        {hasCopiedText ? <Check /> : <Copy />}
+      </p>
+    </div>
+  )
+}
