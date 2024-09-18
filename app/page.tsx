29dbbc85af886b7e07e88c8c17fcc5cb0de0a8ab
wandreperes <wandreperes@yahoo.com.br>
2024-09-18 09:44:48 -0300

    modified:   app/page.tsx 	modified:   components/copy-button.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-      <div className=" relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className='flex min-h-screen w-full flex-col items-center justify-center '>
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] animate-pulse antialiased'
+          className='relative w-64 h-64 dark:drop-shadow-[0_0_0.3rem_#ffffff70] animate-pulse antialiased'
           src='/wpinfo.svg'
           alt='WPInfo Logo'
           width={200}
-          height={50}
-          style={{ height: 'auto' }}
+          height={200}
           priority
         />
       </div>
-      <div className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
+      <div className='grid text-center sm:text-left sm:grid-cols-4'>
         <CopyButton />
       </div>
     </main>
--- components/copy-button.tsx
+++ components/copy-button.tsx
@@ -11,17 +11,14 @@ export function CopyButton() {
   return (
     <div
       onClick={() => copyToClipboard('11974417380')}
-      className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer'
+      className='mt-4 p-4 rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer'
     >
-      <h2 className='mb-3 text-2xl font-semibold'>
-        Contato
-        <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'></span>
-      </h2>
-      <p className='m-0 max-w-[30ch] text-sm opacity-50 flex gap-2 '>
-        <Image src='/whatsapp.png' alt='WhatsApp' height={20} width={20} style={{ width: 'auto' }} /> (11) 9
-        7441-7380
+      <h2 className='mb-2 text-2xl font-semibold'>Contato</h2>
+      <div className='text-sm opacity-50 flex gap-4 justify-center'>
+        <Image className='w-6 h-6' src='/whatsapp.png' alt='WhatsApp' height={20} width={20} priority />
+        <p>(11) 9 7441-7380</p>
         {hasCopiedText ? <Check /> : <Copy />}
-      </p>
+      </div>
     </div>
   )
 }
